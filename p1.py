bd88add1e38f20e49cd02abb311a8624d304e983
alex <prizrakfantezi@gmail.com>
2013-10-10 01:08:26 +0400

    add func offsetPoint, edit import section

--- p1.py
+++ p1.py
@@ -2,14 +2,26 @@
 import os
 
 
+
 def readPoints(fileName):
 	f = open(fileName)
 	points = f.read()
 	return points
-	
+
+def offsetPoint(point, axis, distance):
+	point[axis] = point[axis]+distance
+	return point
+
+
 def palma(args):
+	print (u'Исходне данные')
 	print args.file
 	print args.radius
 	print args.count
 	print args.seg
 	print args.rotate
+	print (u'Точки')
+	print (u'x y z')
+	pointList = readPoints(args.file)
+	print pointList[0]
+        
