08da7900eeada99f89844c9955bc9ccc080ba115
alex <prizrakfantezi@gmail.com>
2013-11-03 12:31:34 +0400

    Дополнил отрисовку граней фигуры

--- draw_stl.py
+++ draw_stl.py
@@ -27,6 +27,13 @@ def createSTLtabe(pointList):
             p2 = pointList[i+1][p]
             p3 = pointList[i+1][p-1]
             result.append([p1,p2,p3])
+            p1 = pointList[i][p]
+            if p==len(pointList[i])-1:
+                p2 = pointList[i+1][0]
+            else:
+                p2 = pointList[i+1][p+1]
+            p3 = pointList[i+1][p]
+            result.append([p1,p2,p3])
             p+=1
         i+=1
     return result
